d2a1e20fe2d435d90a5d26243dbd70f11bfd0083
viktor <sterilework@gmail.com>
2025-09-29 10:45:13 +0300

    feat: add type checking

--- src/math/rectangle.js
+++ src/math/rectangle.js
@@ -4,25 +4,20 @@
 
 // User input
 const i1 = prompt("Введите длину прямоугольника");
-if (isNaN(i1)) {
-    alert("Некорректный ввод: должно быть число")
+const i2 = prompt("Введите ширину прямоугольника");
+if (isNaN(i1) || isNaN(i2)) {
+    alert("Вводимые данные должны быть числами");
 }
 else {
-    const i2 = prompt("Введите ширину прямоугольника");
-    if (isNaN(i2)) {
-        alert("Некорректный ввод: должно быть число")
-    }
-    else {
-        // Rectangle length and width
-        const length = Number(i1);
-        const width = parseFloat(i2);
+    // Rectangle length and width
+    const length = Number(i1);
+    const width = parseFloat(i2);
 
-        // Rectangle area
-        const area = length * width;
-        const perimeter = (length * width) * 2;
+    // Rectangle area
+    const area = length * width;
+    const perimeter = (length * width) * 2;
 
-        // Output
-        console.log(`Rectangle area: ${area}, perimeter: ${perimeter}`);
-        alert(`Площадь прямоугольника: ${area}, периметр: ${perimeter}`)
-    }
+    // Output
+    console.log(`Rectangle area: ${area}, perimeter: ${perimeter}`);
+    alert(`Площадь прямоугольника: ${area}, периметр: ${perimeter}`)
 }
