50242c156bd87fd0e97956393b3e01dd99f4d99e
viktor <sterilework@gmail.com>
2025-09-29 10:25:32 +0300

    feat: add rectangle calculation logic

---
+++ src/math/rectangle.js
@@ -0,0 +1,14 @@
+/**
+ * Rectangle parameters calculation
+ */
+
+// Rectangle length and width
+const length = 10;
+const width = 5;
+
+// Rectangle area
+const area = length * width;
+const perimeter = (length * width) * 2;
+
+// Output
+console.log(`Rectangle area: ${area}, perimeter: ${perimeter}`);
