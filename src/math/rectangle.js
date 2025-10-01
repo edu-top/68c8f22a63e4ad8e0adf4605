c3892f1ead0f641444833b9bc8855e4adee8242b
viktor <sterilework@gmail.com>
2025-10-01 05:44:58 +0300

    sample: add checking length and width positivity

--- src/math/rectangle.js
+++ src/math/rectangle.js
@@ -5,14 +5,16 @@
 // User input
 const i1 = prompt("Введите длину прямоугольника");
 const i2 = prompt("Введите ширину прямоугольника");
-if (isNaN(i1) || isNaN(i2)) {
-    alert("Вводимые данные должны быть числами");
-}
-else {
-    // Rectangle length and width
-    const length = Number(i1);
-    const width = parseFloat(i2);
 
+// Parse input
+const length = Number(i1);
+const width = parseFloat(i2);
+
+if (isNaN(length) || isNaN(width))
+    alert("Вводимые данные должны быть числами");
+else if (length < 0 || width < 0)
+    alert("Введенные числа должны быть положительными")
+else {
     // Rectangle area
     const area = length * width;
     const perimeter = (length * width) * 2;
